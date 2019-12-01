cb59a4e2eca289639f2a12f51727bfc4753d49cc
Eonist <30n1st@gmail.com>
2019-12-02 00:22:16 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ThemePark",
+    platforms: [.iOS(.v12), .macOS(.v10_13)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
